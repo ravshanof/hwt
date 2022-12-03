7ee67037929ee71f15e2aa995788cdfdf9fddbdb
samandarco <ravshanoff@protonmail.com>
2022-12-03 19:29:41 +0500

    Update example.ts

--- example.ts
+++ example.ts
@@ -1,11 +1,13 @@
 import hwt from './mod.ts';
 
-Deno.env.set("HEX_KEY", await hwt.generateHex());
+// Deno.env.set("HEX_KEY", await hwt.generateHex());
 
 const HEX_KEY = Deno.env.get("HEX_KEY");
 
 
 const jwt = await hwt.sign(HEX_KEY, {"username": "samandarco"});
+
+
 const payload = await hwt.verify(jwt, HEX_KEY);
 
 console.log(HEX_KEY)
